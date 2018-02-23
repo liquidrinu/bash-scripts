195bcb3ae6cfb4e3309179a0bd0e02dc706f2003
liquidrinu <liquidrinu@users.noreply.github.com>
2018-02-23 02:33:11 +0100

    Update partycrasher.sh

--- partycrasher.sh
+++ partycrasher.sh
@@ -110,9 +110,18 @@ echo -e "\033[0m";
 # development
 if [ $DEV = true ] 
   then
-    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-    apt-get install -y nodejs && apt-get install -y npm
-    npm install npm@latest -g
+    ## curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+    ## apt-get install -y nodejs && apt-get install -y npm
+    ## npm install npm@latest -g
+    if [ $USER = true ]
+    then
+    runuser -l $uservar -c \
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+    nvm install node
+    else 
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+    nvm install node
+    fi
 fi
 
 ## initialize ssh server
