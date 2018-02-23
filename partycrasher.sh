6d3c58ec47edb7b2772a311f8224d2922fa33475
liquidrinu <liquidrinu@users.noreply.github.com>
2018-02-23 03:48:32 +0100

    Update partycrasher.sh

--- partycrasher.sh
+++ partycrasher.sh
@@ -114,8 +114,9 @@ if [ $DEV = true ]
     if [ $USER = true ]
     then
     scriptvar="https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh"
-    runuser -l "$uservar" -c "curl -o- "$scriptvar" | bash"
-    su "$uservar" && "nvm install node"
+    su "$uservar"
+    curl -o- "$scriptvar" | bash
+    nvm install node
     else 
     curl -o- "$scriptvar" | bash
     nvm install node
