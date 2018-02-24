8dd7a5a2ca10d2c7ad9924983a39ec24ecd65c27
liquidrinu <liquidrinu@users.noreply.github.com>
2018-02-24 03:54:03 +0100

    Update partycrasher.sh

--- partycrasher.sh
+++ partycrasher.sh
@@ -3,32 +3,29 @@
 # insert packages
 APT=('sudo' 'htop' 'git' 'curl' 'wget' 'mpv' 'net-tools');
 
-USERVAR=false
+#USERVAR=false
 USER=false
 PASSWD=false
 DISTRO=false
 SSH=false
-DEV=false
+#DEV=false
 PROFILE=false
 
 # Root privileges
-echo -e "\e[96m";
-[ `whoami` = root ] || exec su -c $0 root
+echo -e "\\e[96m";
+[ "$(whoami)" = root ] || exec su -c "$0" root
 
 # adduser
-echo -e "\e[38;5;82m"
-read -p "Add user? [y/N] " var1
+echo -e "\\e[38;5;82m"
+read -p  "Add user? [y/N] " var1
 
-if [ "$var1" = "y" ]
-  then
+if [ "$var1" = "y" ]; then
     echo "Create user + home directory"
 
-while [ $USER = false ]
-  do
-    read -p "Username: " uservar
+while [ $USER = false ]; do
+    read -p  "Username: " uservar
 
-    if [ `id -u $uservar 2>/dev/null || echo -1` -ge 0 ]
-      then
+    if [ "$(id -u "$uservar" 2>/dev/null || echo -1)" -ge 0 ]; then
         echo "user already exists!"
       else
         sudo useradd -m -d "/home/$uservar" -s /bin/bash -U "$uservar"
@@ -38,43 +35,39 @@ while [ $USER = false ]
 done
 
 # set password
-while [ $PASSWD = false ]
-do
-    CHECK=$(passwd --status $uservar | awk '{print $2}')
-
-    if [ "$CHECK" = "P" ]
-    then
-        PASSWD=true
-    else
-        sudo passwd "$uservar"
-    fi
+while [ "$PASSWD" = "false" ]; do
+  CHECK=$(passwd --status "$uservar" | awk '{print $2}')
+  
+  if [ "$CHECK" = "P" ]; then
+    PASSWD=true
+  else
+    sudo passwd "$uservar"
+  fi
 done
 fi
 
 # sources
-read -p "Do you want clean install of Debian Stretch? [y/N]) " var2
+read -p  "Do you want clean install of Debian Stretch? [y/N]) " var2
 
-if [ "$var2" = "y" ]
-  then
-      DISTRO=true
+if [ "$var2" = "y" ]; then
+  DISTRO=true
 fi
 
 # openSSH
-read -p "Do you want openSSH? [y/N] " var3
-if [ "$var3" = "y" ]
-  then
+read -p  "Do you want openSSH? [y/N] " var3
+if [ "$var3" = "y" ]; then
       SSH=true
 fi
 
 # custom packages (profile)
   echo -e ""
-  echo -e "\e[38;5;82mCurrently set: \e[95m ${APT[@]}\e[38;5;82m"
-  read -p "Install? [y/N]" profile
-  echo -e "\e[95m"
+  echo -e "\\e[38;5;82mCurrently set: \\e[95m $*{APT[@]} \\e[38;5;82m"
+  
+  read -p  "Install? [y/N]" profile
+  echo -e "\\e[95m"
 
-if [ "$profile" = "y" ]
-  then
-      PROFILE=true
+if [ "$profile" = "y" ]; then
+  PROFILE=true
 fi
 
 # dev packages
@@ -87,10 +80,9 @@ fi
 #  fi
 #fi
 
-if [ $DISTRO = true ]
-  then
-    cat /etc/apt/sources.list > sources.bkup1
-    rm  /etc/apt/sources.list
+if [ $DISTRO = true ]; then
+  cat /etc/apt/sources.list > sources.bkup1
+  rm  /etc/apt/sources.list
 
 SOURCE=$(cat <<EOF
 deb http://deb.debian.org/debian stretch main
@@ -106,27 +98,24 @@ EOF
 echo "$SOURCE" > "/etc/apt/sources.list"
 fi
 
-if [ "$PROFILE" = "true" ]
-then
-for i in {14..21} {21..76} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done ; echo
+if [ "$PROFILE" = "true" ]; then
+for i in {14..21} {21..76} ; do echo -en "\\e[38;5;${i}m#\\e[0m" ; done ; echo
+
 # init
-    apt-get update && apt-get upgrade -y
-    apt autoclean -y  && apt autoremove -y
+  apt-get update && apt-get upgrade -y
+  apt autoclean -y  && apt autoremove -y
 
 # personal profile packages
-echo -e "\e[38;5;198m \n"
-echo -e "Profile Packages"
-echo -e "\n"
-    apt-get install ${APT[@]} -y
+echo -e "\\e[38;5;198m \\n"
+echo -e "Profile Packages" + "\\n"
+  apt-get install "${APT[@]}" -y
 fi
-echo -e "\033[0m";
+echo -e "\\033[0m";
 
 ## initialize ssh server
-if [ "$SSH" = "true" ]
-  then
+if [ "$SSH" = "true" ]; then
   apt-get install openssh-server -y && service ssh start
-  if [ "$USER" = "true" ]
-  then
+  if [ "$USER" = "true" ]; then
 SSH_CREDS=$(cat <<EOF
 
 Port 22
@@ -154,12 +143,22 @@ fi
 
 # summary
 localip=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
-echo -e "\e[93m";
+echo -e "\\e[93m";
 
-if [ "$USER" = "true" ] 
-  then
+if [ "$USER" = "true" ]; then
   echo -e "user :  $uservar"
   echo -e "home :  /home/$uservar"
 fi
   echo -e "host :  $localip"
-echo -e "\033[0m";
+echo -e "\\033[0m";
+
+# set alias
+read -p "Set Alias? [y/N]" aliasvar
+if [ "$aliasvar" = "y" ]; then
+  if [ -f /etc/bash.bashrc ]; then
+    echo "alias skaffa='ls -l'" >> /etc/bash.bashrc
+  #if [ "$USER" = "true"]; then
+  #  echo ""  echo $PWD
+  #fi
+  fi
+fi
